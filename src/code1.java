803956bc44ad1ab7c6af69a67497a784ec057ee7
Ganesh Dutt <gdutt2304@gmail.com>
2025-06-03 14:31:59 +0530

    first commit

---
+++ src/code1.java
@@ -0,0 +1,33 @@
+public class code1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world ");
+
+//        int a = 23;
+//        int b = 56;
+//        int c = a + b;
+//        System.out.println(c);
+//        String str  = "Ganesh Dutt";
+//        System.out.println(str);
+
+//       int  data = 45;
+
+    }
+}
+//int a = 5773287;
+//1 byte  - 8bit           range  = -128 to 127
+//2 short  = 16bit         range  =-2^15 to (2^15)-1
+//4 int  = 32bit
+//8 long  = 64bit
+
+
+//float a = 43343.7787
+//float  = 32 bit
+//double = 64bit
+
+//Character Type
+//char a = 'G';
+
+//Boolean
+//boolean  a = true
+
+
---
+++ src/code2.java
@@ -0,0 +1,62 @@
+public class code2 {
+    public static void main(String[] args) {
+//        int a = 56;
+//        int b = 45;
+//        int c = a + b;
+//        System.out.println(c);
+
+        //System.out.println(a++); // 56++ // 57
+       // System.out.println(a--); // 56++ // 57
+//        System.out.println(a--);
+//        System.out.println(a);
+
+
+//        System.out.println(b--);
+//        System.out.println(b);
+
+
+//        int a = 78;
+//        int b = 78;
+
+//        System.out.println(a == b);
+//        System.out.println(a > b);
+//        System.out.println(a < b);
+//        System.out.println(a <= b);
+//        System.out.println(a >= b);
+//        System.out.println(!(a == b));
+
+//        AND  &&, OR ||, NOT !
+
+int  a = 44;
+String output  = (a % 2 == 0)?"Even":"ODD";
+        System.out.println(output);
+
+    }
+}
+//                a, b
+//                a = true; ✔️
+//                b = false;  ❌  //5min
+//             time =   1PM
+ //true  = 1
+ //false  = 0
+
+//AND
+//a    b   = result
+//1    0   = 0
+//0    1   = 0
+//0    0   = 0
+//1    1   = 1
+
+
+// OR ||
+//a    b   = result
+//1    0   = 1
+//0    1   = 1
+//0    0   = 0
+//1    1   = 1
+
+//!(true) = false
+
+
+//(codition)?true:false
+
