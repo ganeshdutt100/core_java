e1036b5ab1782e7ec68d10193cb8b21cc4823cf9
Ganesh Dutt <gdutt2304@gmail.com>
2025-06-04 13:46:40 +0530

    Update if else

---
+++ src/code3.java
@@ -0,0 +1,29 @@
+public class code3 {
+    public static void main(String[] args) {
+        //if
+        // if else
+//        int a = 61;
+//        if(a % 2 == 0){
+//            System.out.println("Even" );
+//        }else{
+//            System.out.println("Odd");
+//        }
+
+//        int marks  = 30;
+//        if(marks >= 60) {
+//            System.out.println("First");
+//        }
+//        else if(marks >= 40){
+//            System.out.println("Second ");
+//        }
+//        else if(marks >= 30){
+//            System.out.println("Third");
+//        }
+//        else{
+//            System.out.println("Fail");
+//        }
+
+
+
+    }
+}
