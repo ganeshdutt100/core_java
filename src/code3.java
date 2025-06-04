25d9ca5df2e4c2f7fd28adba0b8fca4e3d78f50f
Ganesh Dutt <gdutt2304@gmail.com>
2025-06-04 14:14:00 +0530

    Update Switch and Nested

--- src/code3.java
+++ src/code3.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class code3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         //if
         // if else
 //        int a = 61;
@@ -23,7 +26,54 @@ public class code3 {
 //            System.out.println("Fail");
 //        }
 
+// nested if else
+// int recharge = 250;
+// int youTubePremium = 3000;
+//
+// if(recharge >= 350){
+//     System.out.println("Recharge is Valid ");
+//     if(youTubePremium >= 300){
+//         System.out.println("YouTube Premium is activated ");
+//     }
+//     else{
+//         //stm
+//         System.out.println("Recharge don but YouTube Premium not activated ");
+//     }
+// }
+// else{
+//     //stm
+//     System.out.println("Recharge amount is to low");
+// }
+//
+        System.out.println("Enter a number (1  to 7) :  ");
+        int Days  = sc.nextInt();
+        switch (Days){
+            case 1 :
+                System.out.println("Monday");
+                break;
+                case 2 :
+                System.out.println("Tuesday");
+                break;
+                case 3:
+                System.out.println("Wednesday");
+                break;
+                case 4 :
+                System.out.println("Thursday");
+                break;
+                case 5 :
+                System.out.println("Friday");
+                break;
+                case 6 :
+                System.out.println("Saturday");
+                break;
+                case 7 :
+                System.out.println("Sunday");
+                break;
 
+            default:
+                System.out.println("invalid day");
+
+        }
 
     }
 }
